b3dc5a32c41b6e849a0c03c452a8db236feefc0d
haomuren <827172670@qq.com>
2019-02-21 11:15:47 +0800

    wrote a gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+//gulp的配置文件
+var gulp = require("gulp");//加载gulp模块 -->引入装好的gulp文件
+var connect = require("gulp-connect")//服务器模块
+var sass = require("gulp-sass")//将sass文件转换为css的插件 模块
+var sourcemaps = require("gulp-sourcemaps")//让编译后的文件和源文件发生关联的模块
+var concat = require("gulp-concat");//合并文件 --合并只是放一起--压缩才会真正合并相同样式
+var uglify = require("gulp-uglify");
+var rename = require("gulp-rename");////设置压缩后的文件名
+var babel = require("gulp-babel");
+var cleancss = require("gulp-clean-css");//压缩css文件
+
+//调用gulp的task方法创建任务
+
+gulp.task("copyIndex",function(){
+	gulp.src("*.html")
+	.pipe(gulp.dest("dist"))
+	.pipe(connect.reload());//浏览器自动刷新
+});
+
+
+gulp.task("copyImg",function(){
+	gulp.src("img/*.{jpg,png}")
+	.pipe(gulp.dest("dist/img"));
+})
+
+
+gulp.task("copyJs",function(){
+	gulp.src("js/*.js")
+	.pipe(gulp.dest("dist/js"));
+})
+
+
+gulp.task("copyCss",function(){
+	gulp.src("css/*.css")
+	.pipe(gulp.dest("dist/css"));
+})
+
+
+gulp.task("watch",function(){
+	gulp.watch("*.html",["copyIndex"]);
+	gulp.watch("css/*.css",["csscompress"]);
+})
+
+
+gulp.task("server",function(){
+	connect.server({
+		root:"dist",
+		livereload:true,
+	});
+})
+
+
+gulp.task("default",["server","watch"]);
+
+
+gulp.task('scripts',function(){
+	return gulp.src("js/*.js")//所有is下的js文件
+	.pipe(concat('mix.js'))//合并的文件名  想好写
+	.pipe(gulp.dest('dist/js'))
+	.pipe(uglify())//压缩
+	.pipe(rename('mix.min.js'))//压缩以后文件名更名
+	.pipe(gulp.dest('dist/js')); 
+})
+
+
+
+gulp.task('csscompress',function(){ 
+	return gulp.src("css/*.css")//所有css下的css文件
+	.pipe(concat('mix.css'))//合并的文件名  想好写
+	.pipe(gulp.dest('dist/css'))
+	.pipe(cleancss())
+	.pipe(rename('mix.min.css'))//压缩以后添加的文件名
+	.pipe(gulp.dest('dist/css'));//再把压缩的文件添加dist
+})
