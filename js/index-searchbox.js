b12ff16edf2394f8359ed228d43d59065c022145
haomuren <827172670@qq.com>
2019-02-24 16:57:22 +0800

    js update

---
+++ js/index-searchbox.js
@@ -0,0 +1,21 @@
+var oTxt = document.getElementById("key");
+var oList = document.getElementById("jiekou");
+oTxt.oninput = function() {
+	var oScript = document.createElement("script");
+	//oScript.src = "https://sp0.baidu.com/5a1Fazu8AA54nxGko9WTAnF6hhy/su?wd="+oTxt.value+"&json=1&cb=aa";
+	oScript.src = "https://suggest.taobao.com/sug?code=utf-8&q=" + oTxt.value + "&callback=aa";
+	document.body.appendChild(oScript);
+	oList.style.display = "inline";
+}
+
+function aa(data) {
+	//console.log(data);
+	data = data.result;
+	var str = "";
+	data.forEach((item) => {
+		//str += "<li><a href=' "+item+"'>"+item+"</a></li>"
+		str += "<li><a href='https://s.taobao.com/search?q=" + item[0] + "'>" + item[0] + "</a></li>"
+	});
+	oList.innerHTML = str;
+}
+
